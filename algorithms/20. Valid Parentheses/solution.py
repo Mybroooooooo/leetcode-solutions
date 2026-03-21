d9e09ce250cfdf81cedfad7f0ef4fc756f465493
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-22 01:22:40 +0300

    Update solution.py

--- algorithms/20. Valid Parentheses/solution.py
+++ algorithms/20. Valid Parentheses/solution.py
@@ -1,1 +1,16 @@
-
+def isValid(self, s: str) -> bool:
+        stack = []
+        for i in range(0, len(s)):
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if s[i] == ')' and top != '(':
+                    return False
+                if s[i] == '}' and top != '{':
+                    return False
+                if s[i] == ']' and top != '[':
+                    return False
+        return len(stack) == 0
