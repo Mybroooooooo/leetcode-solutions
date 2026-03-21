e84e63b1333da60a2468edeed4875c64452de971
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-22 01:15:51 +0300

    Update solution.py

--- algorithms/9. Palindrome Number/solution.py
+++ algorithms/9. Palindrome Number/solution.py
@@ -1,1 +1,10 @@
+def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        copyX = x
+        reverse = 0
 
+        while x>0:
+            reverse = (reverse*10) + (x%10)
+            x //=10
+        
+        return reverse==copyX
