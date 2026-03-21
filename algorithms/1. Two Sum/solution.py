38b0180cdf567a761c66c0ef51365f61f40fa64d
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-22 00:50:54 +0300

    Update solution.py

--- algorithms/1. Two Sum/solution.py
+++ algorithms/1. Two Sum/solution.py
@@ -1,1 +1,7 @@
-
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        already_seen = {}
+        for i, num in enumerate(nums):
+            need = target - num
+            if need in already_seen:
+                return [already_seen[need], i]
+            already_seen[num] = i
