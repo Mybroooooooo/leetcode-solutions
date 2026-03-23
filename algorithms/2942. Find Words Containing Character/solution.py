1924ec2020b8c989b018481f06a7da5b97ead654
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-23 15:48:25 +0300

    Update solution.py

--- algorithms/2942. Find Words Containing Character/solution.py
+++ algorithms/2942. Find Words Containing Character/solution.py
@@ -1,1 +1,7 @@
-
+def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result = []
+        for i in range(len(words)):
+            if x in words[i]:
+                result.append(i)
+        
+        return result
