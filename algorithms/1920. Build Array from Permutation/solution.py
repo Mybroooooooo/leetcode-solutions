7648321444feb7618341af10651f218acd88cab9
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-25 19:47:31 +0300

    Update solution.py

--- algorithms/1920. Build Array from Permutation/solution.py
+++ algorithms/1920. Build Array from Permutation/solution.py
@@ -1,1 +1,8 @@
+    def buildArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] += (1024*(nums[nums[i]] % 1024))
+        
+        for i in range(len(nums)):
+            nums[i] //= 1024
 
+        return nums 
