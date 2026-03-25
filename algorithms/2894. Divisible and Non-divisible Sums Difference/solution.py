c6d4dc4cc72acb97e75127b1beeb820c46c937c4
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-25 19:52:24 +0300

    Update solution.py

--- algorithms/2894. Divisible and Non-divisible Sums Difference/solution.py
+++ algorithms/2894. Divisible and Non-divisible Sums Difference/solution.py
@@ -1,1 +1,7 @@
-
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1, num2 = 0, 0
+        for i in range(n+1):
+            if i % m == 0:
+                num2 += i
+            else: num1 += i
+        return num1-num2
