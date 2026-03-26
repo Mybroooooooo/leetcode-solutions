5e71d6454cfc6c071d6ac6880caded351f27393c
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-26 22:00:51 +0300

    Update solution.py

--- Easy/3194. Minimum Average of Smallest and Largest Elements/solution.py
+++ Easy/3194. Minimum Average of Smallest and Largest Elements/solution.py
@@ -1,1 +1,16 @@
-
+def minimumAverage(self, nums: List[int]) -> float:
+        averages = []
+        while len(nums) > 0:
+            max_ind = 0
+            min_ind = 0
+            for i in range(len(nums)):
+                if nums[i] > nums[max_ind]:
+                    max_ind = i
+                if nums[i] < nums[min_ind]:
+                    min_ind = i
+            a = (nums[min_ind] + nums[max_ind])/2
+            for idx in sorted([min_ind, max_ind], reverse=True):
+                nums.pop(idx)
+            averages.append(a)
+        
+        return min(averages)
