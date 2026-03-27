205d5c2fc919aebc865ec4f15d73e270c01d2bf6
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-27 21:30:43 +0300

    Update solution.py

--- Easy/14. Longest Common Prefix/solution.py
+++ Easy/14. Longest Common Prefix/solution.py
@@ -1,1 +1,14 @@
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        answer = ''
+        min_len = min(len(word) for word in strs)
 
+        for pos in range(min_len):
+            current = strs[0][pos]
+
+            for i in range(1, len(strs)):
+                if strs[i][pos] != current:
+                    return answer
+            
+            answer += current
+        
+        return answer
