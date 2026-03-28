deeca216e80568cc08aff0db0fabeba68ad6e651
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-03-29 01:45:10 +0300

    Update solution.sql

--- Easy/1050. Actors and Directors Who Cooperated At Least Three Times/solution.sql
+++ Easy/1050. Actors and Directors Who Cooperated At Least Three Times/solution.sql
@@ -1,1 +1,6 @@
-
+select actor_id, director_id from (
+    select actor_id, director_id,
+    count(timestamp) as cooperated
+    from ActorDirector
+    group by actor_id, director_id
+) answer_tabel where cooperated >= 3;
