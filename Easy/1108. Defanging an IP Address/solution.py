fcd9c6d912a34ea19956e2cc3fc567f1c1e67695
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-04-01 18:15:18 +0300

    Create solution.py

---
+++ Easy/1108. Defanging an IP Address/solution.py
@@ -0,0 +1,8 @@
+def defangIPaddr(self, address: str) -> str:
+        result = ''
+        for i in address:
+            if i == '.':
+                result += '[.]'
+            else:
+                result += i
+        return result
