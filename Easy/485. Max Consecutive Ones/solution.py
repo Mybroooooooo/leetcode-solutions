10a8611503fe40eb927c7ca181204556dbd61f53
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-04-22 13:18:13 +0300

    Create solution.py

---
+++ Easy/485. Max Consecutive Ones/solution.py
@@ -0,0 +1,11 @@
+def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res = 0
+        cur = 0
+        for i in nums:
+            if i:
+                cur += 1
+                if cur > res:
+                    res = cur
+            else:
+                cur = 0
+        return res
