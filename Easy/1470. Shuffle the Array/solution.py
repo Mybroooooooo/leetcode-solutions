90cf8de340b891dcc77a048bbb480d31333a193e
Mybroooooooo <134226788+Mybroooooooo@users.noreply.github.com>
2026-04-22 13:13:54 +0300

    Create solution.py

---
+++ Easy/1470. Shuffle the Array/solution.py
@@ -0,0 +1,10 @@
+def shuffle(self, nums: List[int], n: int) -> List[int]:
+        l = []
+        l1 = nums[:n]
+        l2 = nums[n:]
+
+        for i in range(n):
+            l.append(l1[i])
+            l.append(l2[i])
+        
+        return l
